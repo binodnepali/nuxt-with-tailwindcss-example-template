99816ea766a2f7da5831f9ae846033f6c3fe7791
Binod Nepali <nepalibinod9@gmail.com>
2022-11-02 20:43:22 +0100

    style: apply tailwindcss class in home page title

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
+    <h1 class="text-3xl font-bold">Tailwind Class</h1>
     <p>Glad that you had try my template</p>
   </div>
 </template>
